4096e2449a1301765092bfa77c7d9a27ab81f162
sheriffcodes <sheriffcodes01@gmail.com>
2023-05-04 10:32:58 +0100

    creating a dynamic value

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp({
-    //data, functions
-    //template: '<h2>I am the template</h2>'
+    data() {
+        return {
+            title: 'The final empire',
+            author: "Adewole",
+            age: 45
+        }
+    }
 })
 
 app.mount('#app')
