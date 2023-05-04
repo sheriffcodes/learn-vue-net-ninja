9c02318783a362375162760ced16c1d705998489
sheriffcodes <sheriffcodes01@gmail.com>
2023-05-04 13:53:30 +0100

    mouse events

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The final empire',
             author: "Adewole",
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
-        toggleShowBooks() {
-            // a way to set it to the opposite - true/false
-            this.showBooks = !this.showBooks
+        toggleShowBooks(){
+            this.showBooks = !showBooks
+        },
+        // outputting the event object and also trying a direct argument(data)
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if(data) {
+                console.log(data)
+            }
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 
